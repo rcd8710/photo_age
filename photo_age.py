c3744031d3ed361fcdbf6ffbbee264715d219d14
Rited Das <rcd8710@g.rit.edu>
2024-03-04 03:36:52 -0500

    completed program

--- photo_age.py
+++ photo_age.py
@@ -1,27 +1,40 @@
+#This program takes an image from the img folder and converts it to a picture you would see in the 60s
+#or so. It does this via the Pillow library and its image features.
+
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
 path = './imgs'
-pathOut = '/editedImgs'
+pathOut = './editedImgs'  # Adjust the output path accordingly
+
+# Create output directory if it doesn't exist
+os.makedirs(pathOut, exist_ok=True)
+
 for filename in os.listdir(path):
-    #store image in img
+    # Store image in img
     img = Image.open(f"{path}/{filename}")
 
-    #sharpen the image
-    edit = img.filter(ImageFilter.SHARPEN).convert('L')
+    # Decrease sharpness by applying blur
+    edit = img.filter(ImageFilter.BLUR).convert('L')
 
-    #image contra st
-    img_enhance = ImageEnhance.contrast(edit)
+    # Image contrast
+    img_enhance = ImageEnhance.Contrast(edit)
     edit = img_enhance.enhance(1.5)
-   
-    #brightness
+
+    # Brightness
     img_enhance = ImageEnhance.Brightness(edit)
-    edit = img_enhance(.8)
-    
-    #change image saturation
-    img_enhance = ImageEnhance.color(edit)
-    edit = img_enhance(.4)
+    edit = img_enhance.enhance(0.8)
 
+    # Change image saturation
+    img_enhance = ImageEnhance.Color(edit)
+    edit = img_enhance.enhance(0.4)
 
-    clean_name = os.path.splittext(filename)[0]
-    edit.save(f'{path}/{clean_name}")_edited.jpg')
+    # Add grain (Gaussian Blur)
+    grain_radius = 1
+    edit = edit.filter(ImageFilter.GaussianBlur(radius=grain_radius))
+
+    # Split filename and extension
+    clean_name, extension = os.path.splitext(filename)
+
+    # Save edited image in the output directory
+    edit.save(f'{pathOut}/{clean_name}_edited.jpg')
